fa42b3fb751a7eeea050a056e8cca472b4384fe2
Yinan Fang <yinan_fang@hotmail.com>
2015-10-06 14:59:21 -0400

    Before switching to jQuery only

--- A2/src/a2.js
+++ A2/src/a2.js
@@ -1,16 +1,59 @@
-var module = angular.module('app', []);
+'use strict';
 
+var module = angular.module('app', []);
 module.controller('container', function($scope) {
   $scope.params = {
     gridLength: {
-      value: 80,
+      value: 10,
       min: 20,
       max: 200,
     },
+    redrawInterval: {
+      value: 0,
+      min: 0,
+      max: 1000,
+    },
+    radiu: {
+      value: 2,
+      min: 1,
+      max: 10,
+    },
+    lonliness: {
+      value: 0,
+      min: 0,
+      max: 10,
+    },
+    overpolulation: {
+      value: 7,
+      min: 0,
+      max: 10,
+    },
+    gmin: {
+      value: 2,
+      min: 0,
+      max: 10,
+    },
+    gmax: {
+      value: 3,
+      min: 0,
+      max: 10,
+    },
   };
+  $scope.range = function(num) {
+    var foo = [];
+    for (var i = 1; i <= num; i++) {
+        foo.push(i);
+    }
+    return foo;
+  };
+
 });
 
+module.directive('displayTable', function($window) {
+  return {
 
+  }
+}
 
 
 
@@ -23,7 +66,20 @@ module.controller('container', function($scope) {
 
 $(document).ready(function(){
   console.log('It works...');
-  // alert('test');
-  // document.write("Hello, Worlddddsss!");
+  var viewPort = $('.display');
+  var viewPortHeight = viewPort.height();
+  var viewPortWidth = viewPort.width();
+  var viewPortSize = Math.min(viewPortHeight, viewPortWidth);
+  var tableSize = viewPortSize*0.9;
+  // var cellSize =
+
+  // Center the table
+  var tableMarginTop = Math.round( (viewPortHeight - tableSize) / 2 );
+  $('.display table').css('margin-top', tableMarginTop);
+
+  // Adjust cell size
+  $('.display tr')
+
+
 });
 
